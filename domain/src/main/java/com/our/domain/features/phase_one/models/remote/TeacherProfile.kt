a28162f00e72aaa7754802e4d5948bbab1d149e7
elroy333 <elroyveb@gmail.com>
2023-01-22 19:50:19 +0200

    update teacher model

--- domain/src/main/java/com/our/domain/features/phase_one/models/remote/TeacherProfile.kt
+++ domain/src/main/java/com/our/domain/features/phase_one/models/remote/TeacherProfile.kt
@@ -2,5 +2,17 @@ package com.our.domain.features.phase_one.models.remote
 
 data class TeacherProfile(
     val teacherAddress: Int,
-    val teacherName: String
+    val teacherAvatar: String,
+    val teacherBirthday: Int,
+    val teacherCreatedDate: String,
+    val teacherId: Int,
+    val teacherLastName: String,
+    val teacherMail: String,
+    val teacherName: String,
+    val teacherPhone: String,
+    val teacherRate: Int,
+    val teacherRating: Int,
+    val teacherSecondPhone: String,
+    val teacherSex: Int,
+    val teacherUpdateDate: String
 )
